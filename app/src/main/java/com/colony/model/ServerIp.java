4eb7ef46e8931b3a72f004f790f8d40102d101e7
zahi111 <zahi111@hotmail.com>
2016-09-25 00:45:55 +0300

    Add UserId on message

--- app/src/main/java/com/colony/model/ServerIp.java
+++ app/src/main/java/com/colony/model/ServerIp.java
@@ -2,6 +2,6 @@ package com.colony.model;
 
 public class ServerIp {
 
-    public final static String server = "http://46.116.43.207/";
+    public final static String server = "xxx.xxx.xxx.xx";
 
 }
